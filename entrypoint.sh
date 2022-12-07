d76580d79952f6841c453bb3ed37ef452b19752c
Kasra Bigdeli <kasra85@gmail.com>
2022-12-06 22:34:44 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ if [ x$INPUT_IMAGE != x ]
 then
   caprover deploy --caproverUrl $INPUT_SERVER --appToken $INPUT_TOKEN --appName $INPUT_APP -i $INPUT_IMAGE
 elif [ x$INPUT_BRANCH != x ]
+then
   caprover deploy --caproverUrl $INPUT_SERVER --appToken $INPUT_TOKEN --appName $INPUT_APP -b $INPUT_BRANCH
 else
   caprover deploy --caproverUrl $INPUT_SERVER --appToken $INPUT_TOKEN --appName $INPUT_APP --tarFile ./deploy.tar
